1dd2dab9419711f9a30870a2cf1b90bf6c9e6912
helisoya <oudot.julien@laposte.net>
2024-06-05 13:56:53 +0200

    Init

---
+++ src/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/main', name: 'app_main')]
+    public function index(): Response
+    {
+        $events = ["Event 1","Event 2","Event 3"];
+        return $this->render('main/main.html.twig', [
+            'events' => $events,
+        ]);
+    }
+}
