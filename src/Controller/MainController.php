0ec69b85512902de0aceed3f0d3aea8178d51243
helisoya <oudot.julien@laposte.net>
2024-06-07 12:00:31 +0200

    Login, Logout, Registration, Start Profile Edit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -12,7 +12,7 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        $events = ["Event 1","Event 2","Event 3"];
+        $events = ["Event Pierre Engine","Event Dorian & Fouzi's Quest","Event Hello World","Event Justin Games"];
         return $this->render('main/main.html.twig', [
             'events' => $events,
         ]);
---
+++ src/Form/EditProfileFormType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class EditProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email',EmailType::class ,['constraints'=> [new Email([
+                'message' => 'Le mail n\'est pas valide.',
+            ])]])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passes doivent être identiques.',
+                'options' => [
+                    'attr' => ['class' => 'password-field'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Entrez un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 8,
+                            'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                        new Regex([
+                            'pattern' => '/\d+/i',
+                            'message'=> 'Vous devez utiliser au moins un nombre'
+                        ]),
+                    ],
+                ],
+                'required' => false,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répeter mot de passe'],
+
+            ])
+            ->add('nom', TextType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez votre nom',
+                    ]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez votre prenom',
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
