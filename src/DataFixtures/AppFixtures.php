b2bcc6bed97419032945a4b4b67a416f9e5525a9
helisoya <oudot.julien@laposte.net>
2024-06-08 18:02:09 +0200

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Event;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
@@ -26,7 +27,34 @@ class AppFixtures extends Fixture implements FixtureGroupInterface, OrderedFixtu
         $user->setNom('Pierre');
         $user->setPrenom('Dupont');
 
+        $user2 = new User();
+        $user2->setEmail('fouzi@pierresoftwares.com');
+        $user2->setRoles(['ROLE_USER']);
+        $user2->setPassword($this->passwordHasher->hashPassword($user2, '12345678'));
+        $user2->setNom('Fouzi');
+        $user2->setPrenom('Miloud');
+
+        $event = new Event();
+        $event->setTitle("Pierre Engine Reveal 1.0");
+        $event->setDescription("Reveal de la version 1.0 de Pierre Engine.");
+        $event->setIsPublic(true);
+        $event->setMaxParticipants(50);
+        $event->setStartDate(new \DateTime("2024-08-08 20:10"));
+        $event->addParticipant($user);
+
+        $event2 = new Event();
+        $event2->setTitle("Pierre Engine Reveal 1.1");
+        $event2->setDescription("Reveal de la version 1.1 de Pierre Engine.");
+        $event2->setIsPublic(false);
+        $event2->setMaxParticipants(78);
+        $event2->setStartDate(new \DateTime("2024-09-08 20:10"));
+        $event2->addParticipant($user);
+        $event2->addParticipant($user2);
+
         $manager->persist($user);
+        $manager->persist($user2);
+        $manager->persist($event);
+        $manager->persist($event2);
         $manager->flush();
     }
 
